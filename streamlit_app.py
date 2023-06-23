f6e194ffdb323cb999bd85159f1f9de973066136
jwentzwork <137341490+jwentzwork@users.noreply.github.com>
2023-06-23 08:48:22 -0500

    Removed json display

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,4 +25,8 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+
+# Normalize the results
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# Show in a dataframe
+streamlit.dataframe(fruityvice_normalized)
